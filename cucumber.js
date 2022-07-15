4d83ec35ba64df6d329bdfef1ba7fa022a7bcfd4
Nikos Karavasilis <nikoskaravasilis10@gmail.com>
2022-07-15 02:40:09 +0200

    remove report generation for remote testing

--- cucumber.js
+++ cucumber.js
@@ -1,6 +1,5 @@
 let cucumberConfig= [
     "--require ./step-definitions/*.ts",
-    "--format json:reports/result.json",
     "--require-module ts-node/register",
     "--publish-quiet"
 ].join(" ");
